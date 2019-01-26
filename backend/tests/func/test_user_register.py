479c57f6b110b2f7c81fd10b7bd2ab56036a526c
stanislawK <stanislaw.krupienko@gmail.com>
2019-01-26 21:38:11 +0100

    add conftest and user register test

---
+++ backend/tests/func/test_user_register.py
@@ -0,0 +1,48 @@
+import pytest
+
+
+def test_register_valid_user(client, new_user):
+    """
+    GIVEN test client and valid user data
+    WHEN post request sent to register endpoint
+    THEN check status 201 and response message
+    """
+    rv = client.post('/register', json=new_user)
+    response = rv.get_json()
+    assert rv.status == '201 CREATED'
+    assert response['message'] == 'User created successfully'
+
+
+def test_register__user_two_times(client, new_user):
+    """
+    GIVEN test client and valid user data
+    WHEN post request sent to register endpoint two times with these same user
+    THEN check status 400 and error message
+    """
+    rv = client.post('/register', json=new_user)
+    response = rv.get_json()
+    assert rv.status == '201 CREATED'
+
+    rv = client.post('/register', json=new_user)
+    response = rv.get_json()
+    assert rv.status == '400 BAD REQUEST'
+    assert response['message'] == 'User with that username already exist'
+
+
+def test_register_with_invalid_email(client, new_user):
+    """
+    GIVEN test client and user with invalid email, and without
+    WHEN post request sent to register endpoint
+    THEN check status and response
+    """
+    new_user['email'] = 'wrong_email'
+    rv = client.post('/register', json=new_user)
+    response = rv.get_json()['message']['email'][0]
+    assert rv.status == '400 BAD REQUEST'
+    assert response == 'Not a valid email address.'
+
+    del new_user['email']
+    rv = client.post('/register', json=new_user)
+    response = rv.get_json()['message']['email'][0]
+    assert rv.status == '400 BAD REQUEST'
+    assert response == 'Missing data for required field.'
---
+++ backend/tests/unit/test_db.py
@@ -0,0 +1,22 @@
+import pytest
+
+from api.models.user import UserModel
+
+
+def test_add_new_user_to_db(db_session, new_user):
+    """
+    GIVEN mocked db session, and user instance
+    WHEN a new user added to db
+    THEN check the user data from db
+    """
+    assert len(db_session.query(UserModel).all()) == 0
+
+    new_user = UserModel(username=new_user['username'],
+                         password=new_user['password'],
+                         email=new_user['email'])
+
+    db_session.add(new_user)
+
+    user = db_session.query(UserModel).filter_by(username="TestUser").first()
+    assert user.password == "testPass"
+    assert user.email == "test@test.com"
