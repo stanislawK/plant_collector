c8d63183499835276d0498749ed1fd89a8ad6961
stanislawK <stanislaw.krupienko@gmail.com>
2019-01-29 21:40:09 +0100

    add hashing password with passlib

--- backend/api/models/user.py
+++ backend/api/models/user.py
@@ -6,7 +6,7 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
 
     @classmethod
