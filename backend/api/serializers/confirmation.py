179bbf1971fa37146618c17e0fbed3f505b7064a
stanislawK <stanislaw.krupienko@gmail.com>
2019-02-16 19:15:32 +0100

    add description serializer and endpoint

--- backend/api/serializers/confirmation.py
+++ backend/api/serializers/confirmation.py
@@ -4,7 +4,7 @@ from marshmallow import fields, Schema
 class ConfirmaionSchema(Schema):
     class Meta:
         fileds = ('id', 'user', 'expire_at', 'confirmed')
-        load_only = ("user")
+        load_only = ("user",)
         dump_only = ('id', 'expire_at', 'confirmed')
 
 
