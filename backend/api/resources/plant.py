79fe34395e6bf9e7e91b268667e3f86b445c6018
stanislawK <stanislaw.krupienko@gmail.com>
2019-03-17 20:43:01 +0100

    put plant endpotin

--- backend/api/resources/plant.py
+++ backend/api/resources/plant.py
@@ -7,6 +7,7 @@ from api.models.plant import PlantModel
 from api.serializers.plant import plant_schema, plants_schema
 
 CREATED_SUCCESFULLY = "New plant created successfully"
+CHANGED_SUCCESFULLY = "Plant changed successfully"
 PLANT_NOT_FOUND = "Plant not found"
 DELETED = "Plant was deleted"
 
@@ -38,6 +39,27 @@ class Plant(Resource):
         if plant and plant.user_id == user_id:
             return plant_schema.dump(plant), 200
         return {"message": PLANT_NOT_FOUND}, 404
+
+    @classmethod
+    @jwt_required
+    def put(cls, plant_id):
+        user_id = get_jwt_identity()
+        plant = PlantModel.find_by_id(plant_id)
+
+        if plant and plant.user_id == user_id:
+            try:
+                plant_data = plant_schema.load(request.get_json(),
+                                               partial=True)
+            except ValidationError as err:
+                return {"message": err.messages}, 400
+            for key in plant_data.keys():
+                print(key)
+                setattr(plant, key, plant_data[key])
+
+            plant.save_to_db()
+            return {"message": CHANGED_SUCCESFULLY}, 201
+        return {"message": PLANT_NOT_FOUND}, 404
+
 
     @classmethod
     @jwt_required
