f04e1d28f21484c320f2aca59b714fd153b45e04
stanislawK <stanislaw.krupienko@gmail.com>
2019-03-22 18:27:27 +0100

    basic edit plant component

--- backend/api/serializers/description.py
+++ backend/api/serializers/description.py
@@ -3,9 +3,11 @@ from marshmallow import fields, Schema, validate
 
 class DescriptionSchema(Schema):
     class Meta:
-        fields = ('plant', 'content', 'source')
+        fields = ('plant', 'content', 'source', 'id')
         load_only = ('plant',)
+        dump_only = ('id', )
 
+    id = fields.Int()
     content = fields.Str(required=True, validete=[validate.Length(max=2000)])
     source = fields.Str(validate=[validate.Length(max=50)])
 
